03ddd6cd543cbcb683c402e8227cbac37ce64d4d
Benjamin Sonntag <benjamin@octopuce.fr>
2013-10-12 15:52:49 +0200

    [fix] menu for check was incorrect

--- www/menu2.php
+++ www/menu2.php
@@ -6,7 +6,7 @@
 	      _("Rename")=>"s0_name.php?rename=",
 	      _("Metadata")=>"s1_meta.php?name=",
 	      _("Scan")=>"s2_scan.php?name=",
-	      _("Check")=>"s3_check.php?rename=",
+	      _("Check")=>"s3_check.php?name=",
 	      );
 foreach($asub as $n=>$l) {
   if (substr($_SERVER["REQUEST_URI"],1,strlen($l))==$l) 
